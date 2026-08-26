f755537c925561329970f660c8cbb129ca826236
mnomanbh-source <m.noman.bh.com>
2026-08-26 16:41:03 +0500

    yhdhd

--- P1/settings.py
+++ P1/settings.py
@@ -144,6 +144,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
+
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / 'dbbackup'}
 DBBACKUP_HOSTNAME = 'geek'
